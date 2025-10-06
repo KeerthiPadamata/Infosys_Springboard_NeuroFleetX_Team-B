670f344f266651cc87c781217a8b89e96db1dc1c
Keerthi Padamata <158249910+KeerthiPadamata@users.noreply.github.com>
2025-10-06 16:39:59 +0530

    Added Backend files

--- Backend/src/main/java/com/example/Auth/repository/DriverRepository.java
+++ Backend/src/main/java/com/example/Auth/repository/DriverRepository.java
@@ -4,6 +4,7 @@ import com.example.Auth.entity.Driver;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
-public interface DriverRepository extends JpaRepository<Driver, Long> {
+public interface DriverRepository extends JpaRepository<Driver, Long>{
     List<Driver> findByAvailableTrue();
 }
+
