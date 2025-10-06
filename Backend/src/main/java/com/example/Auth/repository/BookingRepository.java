183a214a19fa63305d4ff160de931c87ef091821
Keerthi Padamata <158249910+KeerthiPadamata@users.noreply.github.com>
2025-10-06 16:39:38 +0530

    Added Backend files

--- Backend/src/main/java/com/example/Auth/repository/BookingRepository.java
+++ Backend/src/main/java/com/example/Auth/repository/BookingRepository.java
@@ -3,4 +3,5 @@ package com.example.Auth.repository;
 import com.example.Auth.entity.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BookingRepository extends JpaRepository<Booking, Long> {}
+public interface BookingRepository extends JpaRepository<Booking, Long>{}
+
